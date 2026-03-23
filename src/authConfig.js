a3044504639bcca6c582eb082def943e62324be1
pajeffery <pajeffery@gmail.com>
2026-03-23 08:57:26 +0000

    Update authConfig.js

--- src/authConfig.js
+++ src/authConfig.js
@@ -1,6 +1,6 @@
 const msalConfig = {
     auth: {
-        clientId: "YOUR_APP_CLIENT_ID_FROM_AZURE", 
+        clientId: "a358a2b7-47e7-4a31-ad04-630a7b3fa5cc", 
         // Use 'common' for multi-tenant apps
         authority: "https://login.microsoftonline.com/common", 
         redirectUri: window.location.origin,
