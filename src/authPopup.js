f2317eea57876e38b5d3aa3164915002d5bd42b2
pajeffery <pajeffery@gmail.com>
2026-04-08 22:57:35 +0100

    Update loginPopup call to include specific scopes

--- src/authPopup.js
+++ src/authPopup.js
@@ -35,9 +35,9 @@ function unlockStep2() {
 
 async function signIn() {
     try {
-        const loginResponse = await myMSALObj.loginPopup(loginRequest);
+        const loginResponse = await myMSALObj.loginPopup({ scopes: ["User.Read"] });
         showWelcomeMessage(loginResponse.account);
-        return loginResponse.account; // Return the account for other functions to use
+        return loginResponse.account;
     } catch (error) {
         console.error("Sign-in failed:", error);
         throw error;
@@ -58,43 +58,4 @@ function redirectForAdminConsent() {
     const tenant = "organizations"; 
     const adminConsentUrl = `https://login.microsoftonline.com/${tenant}/v2.0/adminconsent?client_id=${clientId}&redirect_uri=${redirectUri}&scope=https://graph.microsoft.com/.default`;
     window.location.assign(adminConsentUrl);
-}
-
-// THE FIX IS IN THIS FUNCTION
-async function getTokenPopup(request) {
-    let currentAccount = myMSALObj.getAllAccounts()[0];
-    
-    // If not signed in, force a sign in first before trying to get the token
-    if (!currentAccount) {
-        console.warn("No account found, forcing login...");
-        currentAccount = await signIn();
-    }
-    
-    request.account = currentAccount;
-
-    try {
-        // Try getting token silently
-        console.log("Attempting silent token acquisition...");
-        const response = await myMSALObj.acquireTokenSilent(request);
-        return response.accessToken;
-    } catch (error) {
-        console.warn("Silent token failed. Initiating interactive consent popup...", error.errorCode);
-        
-        // If silent fails (usually because of consent_required), force the popup
-        // The key here is passing 'prompt: "consent"' to ensure the Azure screen shows up
-        const interactiveRequest = {
-            scopes: request.scopes,
-            account: currentAccount,
-            prompt: "consent", 
-            loginHint: currentAccount.username // Crucial for routing them to the right admin login
-        };
-        
-        try {
-            const popupResponse = await myMSALObj.acquireTokenPopup(interactiveRequest);
-            return popupResponse.accessToken;
-        } catch (popupError) {
-            console.error("Interactive consent failed. User may have closed the window.", popupError);
-            throw popupError;
-        }
-    }
 }
